5e7aba7a36327027781da30c32ce6e5bb27fe74d
malyarc <jmtang1526@gmail.com>
2020-05-01 09:52:54 -0700

    refractored seeding

--- database/seed.js
+++ database/seed.js
@@ -13,23 +13,17 @@ connection.connect();
 for (var i = 9; i < 100; i++) {
   var productid = (Math.floor(Math.random() * 9) + 2);
   var colorName = faker.commerce.color();
-  var colorURL = faker.image.imageUrl();
+  var randomUrlImgID = (Math.floor(Math.random() * 20) + 1);
+  var colorURL = `https://hrr45fec.s3-us-west-1.amazonaws.com/dummypics/${randomUrlImgID}.jpg`;
 
   var queryString = `INSERT INTO colors (product_id, color_name, color_url) VALUES (${productid} , "${colorName}", "${colorURL}");`;
 
-  connection.query(queryString, () => {});
+  connection.query(queryString, (err) => {
+    if (err) {
+      console.log("there has been an error in adding dummy data");
+    }
+  });
 
 }
 
 //INSERT INTO colors (product_id, color_name, color_url) VALUES ( , "", "");
-
-
-// var randomName = faker.name.findName(); // Rowan Nikolaus
-// var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-// var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-// var randomIMGURL = faker.image.imageUrl();
-// var randomColor = faker.commerce.color();
-// var a = faker.image.imageUrl();
-// //console.log(randomName);
-// console.log(Math.floor(Math.random() * 9) + 2);
-// console.log(randomIMGURL);
