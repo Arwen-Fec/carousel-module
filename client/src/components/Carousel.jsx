a1a6b128f0c0a640a1dac5ea9c80afcd40351663
malyarc <jmtang1526@gmail.com>
2020-05-05 11:50:23 -0700

    created the carousel component and also displays onclick new carousel image. CSS included

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Carousel = (props) => (
-  <div>
-    Carousel Component Init
+
+  <div class = "carousel-track-container">
+    <ul class="carousel-track">
+      {props.images.map((image) => <li><img onClick = {() => props.toggleCarouselImage(image)}
+      class = "carousel-image" src={image}/></li>)}
+    </ul>
   </div>
 );
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 let app = express();
+const db = require('../database/index');
 
 const bodyParser = require('body-parser');
-
-app.use(express.static(__dirname + '/../client/dist'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/api/carousel', (req, res) => {
-
-
+app.use('/:id', express.static('client/dist'))
+app.get('/api/carousel/:id', (req, res) => {
+  console.log('params', req.params);
+  db.getImgByProductId(Number(req.params.id), (err, data) => {
+    if (err) {
+      //console.log(err)
+      res.status(400).send();
+    } else {
+      //console.log(data);
+      //console.log('here')
+      res.status(200).send(data);
+    }
+  });
 });
 
 
